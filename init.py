13727b9b3662d76eeae01be9a227058913e1c7e3
walikrence <969327526@qq.com>
2019-03-22 13:19:44 +0800

    nothing

--- init.py
+++ init.py
@@ -2,17 +2,16 @@ import os
 
 
 for i in range(1,12):
-	# command = "mkdir hw" + str(i)
-	# os.system(command)
-	# command = "mkdir hw" + str(i) + "/pic"
-	# os.system(command)
-	# command = "mkdir hw" + str(i) + "/css"
-	# os.system(command)
-	# command = "mkdir hw" + str(i) + "/js"
-	# os.system(command)
-	# command = "mkdir hw" + str(i) + "/pic"
-	# os.system(command)
-	command = "rm hw" + str(i) + "/index.html"
+	command = "mkdir hw" + str(i)
+	os.system(command)
+	command = "mkdir hw" + str(i) + "/pic"
+	os.system(command)
+	command = "mkdir hw" + str(i) + "/css"
+	os.system(command)
+	command = "mkdir hw" + str(i) + "/js"
+	os.system(command)
+	command = "mkdir hw" + str(i) + "/pic"
 	os.system(command)
 	command = "touch hw" + str(i) + "/hw" + str(i) +".html"
 	os.system(command)
+	pass
